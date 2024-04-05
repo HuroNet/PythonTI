d6dab24fe5775e72821a79b23c1ad8ee6d4eb229
Marcos Hernadez <37634946+marcos-gino95@users.noreply.github.com>
2024-04-04 21:29:46 -0500

    ci: implement ci configuration (#2)
    * ci: implement ci configuration
    
    * feat: ruins the code

--- basic_excersises.py
+++ basic_excersises.py
@@ -6,7 +6,7 @@
 # Escribir una función que tome un carácter y devuelva True si es una vocal,
 # de lo contrario devuelve False.
 def verificar_vocal(leter):
-    vocales = ["a", "e", "i", "o", "u"]
+    vocales =["a", "e", "i", "o", "u"]
     leter = leter.lower()
     if leter in vocales:
         return True
