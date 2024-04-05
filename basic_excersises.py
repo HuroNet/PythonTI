790eea3389543498a197dd2c9434948b244f475f
Huron <charlie.walls97@gmail.com>
2024-04-04 20:27:07 -0500

    lint: run lint

--- basic_excersises.py
+++ basic_excersises.py
@@ -1,16 +1,17 @@
-#basic excersises
-#Name: Carlos Paredes
-#email:carlos.paredes23@hotmail.com
+# basic excersises
+# Name: Carlos Paredes
+# email:carlos.paredes23@hotmail.com
 
 
 # Escribir una función que tome un carácter y devuelva True si es una vocal,
 # de lo contrario devuelve False.
 def verificar_vocal(leter):
-    vocales=["a","e","i","o","u"]
-    leter=leter.lower()
+    vocales = ["a", "e", "i", "o", "u"]
+    leter = leter.lower()
     if leter in vocales:
         return True
     else:
         return False
-    #return leter in vocales ---- solo es esto 
+
+
 print(verificar_vocal("O"))
