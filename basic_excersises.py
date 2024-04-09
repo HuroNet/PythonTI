b3c6eb4a6c000b1817f135a9cccf8b820901c8ef
Huron <charlie.walls97@gmail.com>
2024-04-09 09:53:42 -0500

    fix: error corregido

--- basic_excersises.py
+++ basic_excersises.py
@@ -19,38 +19,47 @@ def verificar_vocal(leter):
 # Definir una función inversa() que calcule la inversión de una cadena.
 # Por ejemplo la cadena "estoy probando" debería devolver la cadena "odnaborp yotse"
 
+
 def inversa(cadena):
-    #palabra=cadena[::-1] ##este es el primer metodo 
-    cadena_invertida=""
+    # palabra=cadena[::-1] ##este es el primer metodo
+    cadena_invertida = ""
     for caracter in cadena:
-        cadena_invertida = caracter+ cadena_invertida
+        cadena_invertida = caracter + cadena_invertida
     return cadena_invertida
 
-#print(inversa("estoy probando"))
+
+# print(inversa("estoy probando"))
 
 
-# Escribir una función sum() y una función multip() que sumen y 
+# Escribir una función sum() y una función multip() que sumen y
 # multipliquen respectivamente todos los números de una lista.
 # Por ejemplo: sum([1,2,3,4]) debería devolver 10 y multip([1,2,3,4]) debería devolver 24.
+
 
 def sum(a):
     suma = 0
     for i in a:
         suma = suma + i
     return suma
+
+
 def multi(b):
-    multi=1
+    multi = 1
     for i in b:
         multi *= i
     return multi
 
+
 # print(sum([1,2,3,4]))
 # print(multi([1,2,3,4]))
 
-# Definir una función generar_n_caracteres() que tome un entero n y devuelva el caracter multiplicado por n. 
-# Por ejemplo: generar_n_caracteres(5, "x") debería devolver "xxxxx" 
+
+# Definir una función generar_n_caracteres() que tome un entero n y devuelva el caracter multiplicado por n.
+# Por ejemplo: generar_n_caracteres(5, "x") debería devolver "xxxxx"
 def generar_n_caracteres(entero, caracter):
-    for i in range(1, entero+1):
-        caracteres=i*caracter
+    for i in range(1, entero + 1):
+        caracteres = i * caracter
     return caracteres
-print(generar_n_caracteres(5,"x"))
+
+
+print(generar_n_caracteres(5, "x"))
