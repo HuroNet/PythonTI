b377ac6a5de3bb4017b00806a7279e55aee71c60
Huron <charlie.walls97@gmail.com>
2024-04-18 09:55:42 -0500

    fix: enter one line

--- basic_excersises.py
+++ basic_excersises.py
@@ -65,7 +65,8 @@ def generar_n_caracteres(entero, caracter):
 
 # print(generar_n_caracteres(5, "x"))
 
-# Definir una función generar_n_caracteres() que tome un entero n y devuelva el caracter multiplicado por n.
+# Definir una función generar_n_caracteres() que tome un entero 
+#n y devuelva el caracter multiplicado por n.
 # Por ejemplo: generar_n_caracteres(5, "x") debería devolver "xxxxx"
 
 
