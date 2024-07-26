28081596e458a53b1658764f5b6073ed0706e998
Huron <charlie.walls97@gmail.com>
2024-07-26 17:55:51 -0500

    add fuction fibonacci_sequenece

--- app/excercises/basic_exercises.py
+++ app/excercises/basic_exercises.py
@@ -721,3 +721,20 @@ def es_numero_perfecto(n):
 
 # Escribe una función que genere una secuencia de
 # números Fibonacci de longitud n.
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+
+    sequence = [0, 1]
+    for i in range(2, n):
+        next_number = sequence[-1] + sequence[-2]
+        sequence.append(next_number)
+    return sequence
+
+# Ejemplo de uso
+n = 10
+print(fibonacci_sequence(n))
