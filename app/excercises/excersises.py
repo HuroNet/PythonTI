675de5282146cf558eeda8a7de6c39962f2f5ab3
Huron <charlie.walls97@gmail.com>
2024-08-12 10:23:30 -0500

    add sime proof to comprove if the fuction is correct

--- app/excercises/excersises.py
+++ app/excercises/excersises.py
@@ -1,8 +1,11 @@
 #1. Two Sum
 def two_sum(nums,target):
     for i in range(len(nums) - 1):
+        # print(nums[i])
         for j in range(i+1,len(nums)):
+            # print(nums[j])
+            # print(nums[i],"+",nums[j],"=",nums[i]+nums[j])
             if (i != j and nums[i] + nums[j] == target):
                 return i,j
     return False
-print(two_sum([2,7,6,44],8))
+# print(two_sum([7,44,2,6],8))
