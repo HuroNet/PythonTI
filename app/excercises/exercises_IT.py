22ff3ce74ed7d9324be1bb17134b533429432ba6
Huron <charlie.walls97@gmail.com>
2024-10-04 13:51:18 -0500

    add exercise of reverse of a string

--- app/excercises/exercises_IT.py
+++ app/excercises/exercises_IT.py
@@ -5,3 +5,12 @@
 #  invierte las palabras sin modificar el orden
 #  de las letras dentro de cada palabra.
 #     Ejemplo: Entrada: "Hola mundo" -> Salida: "mundo Hola"
+
+def invertir_palabras(palabras):
+    # palabras=palabras[::-1]
+    invertidas=""
+    for letra in palabras:
+        invertidas= letra + invertidas
+    return invertidas
+
+print(invertir_palabras("Hola Mundo"))
