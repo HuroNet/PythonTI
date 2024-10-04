450788573b51f8b4c563dc3c24b57bdfbf100268
Huron <charlie.walls97@gmail.com>
2024-10-04 13:13:39 -0500

    primer ejercico

---
+++ app/excercises/exercises_IT.py
@@ -0,0 +1,7 @@
+# Invertir palabras en una oración sin invertir
+#  las letras de cada palabra.
+
+#     Enunciado: Dada una cadena de caracteres,
+#  invierte las palabras sin modificar el orden
+#  de las letras dentro de cada palabra.
+#     Ejemplo: Entrada: "Hola mundo" -> Salida: "mundo Hola"
