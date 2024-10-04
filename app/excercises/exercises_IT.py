3ba331bf08191ef3a9b74fea2f02ac8983d28faa
Huron <charlie.walls97@gmail.com>
2024-10-04 13:51:38 -0500

    add format docuement

--- app/excercises/exercises_IT.py
+++ app/excercises/exercises_IT.py
@@ -6,11 +6,13 @@
 #  de las letras dentro de cada palabra.
 #     Ejemplo: Entrada: "Hola mundo" -> Salida: "mundo Hola"
 
+
 def invertir_palabras(palabras):
     # palabras=palabras[::-1]
-    invertidas=""
+    invertidas = ""
     for letra in palabras:
-        invertidas= letra + invertidas
+        invertidas = letra + invertidas
     return invertidas
 
-print(invertir_palabras("Hola Mundo"))
+
+# print(invertir_palabras("Hola Mundo"))
