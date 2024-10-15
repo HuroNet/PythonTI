7f490af36f81b80109e06763d8c2332e4200da71
Huron <charlie.walls97@gmail.com>
2024-10-15 11:32:43 -0500

    add format docuemnt

--- app/excercises/exercises_IT.py
+++ app/excercises/exercises_IT.py
@@ -349,7 +349,9 @@ def agrupar_elementos(lista):
     for i in range(0, len(lista), 2):
         print(lista[i])
         if i + 1 < len(lista):
-            resultado.append((lista[i], lista[i + 1])) # si se quiere en [] solo se pone y punto
+            resultado.append(
+                (lista[i], lista[i + 1])
+            )  # si se quiere en [] solo se pone y punto
         else:
             resultado.append((lista[i],))
 
