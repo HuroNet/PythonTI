6bdc740745e26518cd05d23e70166f67199c923f
Huron <charlie.walls97@gmail.com>
2024-10-21 11:40:44 -0500

    Terminado

--- app/excercises/exercises_IT.py
+++ app/excercises/exercises_IT.py
@@ -4,3 +4,24 @@
 #     Enunciado: Verifica si una cadena es la misma 
 # al leerla al derecho y al revés. Ignora espacios y mayúsculas.
 #     Ejemplo: Entrada: "Amo la paloma" -> Salida: True
+def palindromo(string):
+    original= string.lower().replace(" ","")
+    invertida=""
+    for i in range(len(string)-1,-1,-1):
+        invertida += string[i].lower().replace(" ","")
+    if original == invertida:
+        return True
+    else:
+        return False
+    
+
+print(palindromo("Amo la paloma"))
+
+
+def invertir_cadena(cadena):
+    cadena_invertida=""
+    for valor in cadena:
+        cadena_invertida = valor + cadena_invertida
+    return cadena_invertida
+
+# print(invertir_cadena("hola mundo"))
