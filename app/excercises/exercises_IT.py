0f936493648e153f92d93f82fc12398fd78905df
Huron <charlie.walls97@gmail.com>
2024-10-21 12:11:47 -0500

    add more exercises

--- app/excercises/exercises_IT.py
+++ app/excercises/exercises_IT.py
@@ -6,16 +6,19 @@
 #     Ejemplo: Entrada: "Amo la paloma" -> Salida: True
 def palindromo(string):
     original= string.lower().replace(" ","")
-    invertida=""
-    for i in range(len(string)-1,-1,-1):
-        invertida += string[i].lower().replace(" ","")
+    print("original",original)
+    invertida=string[::-1].lower().replace(" ","")
+    print("inver",invertida)
+
+    # for i in range(len(string)-1,-1,-1):
+    #     invertida += string[i].lower().replace(" ","")
     if original == invertida:
         return True
     else:
         return False
     
 
-print(palindromo("Amo la paloma"))
+# print(palindromo("Amo la paloma"))
 
 
 def invertir_cadena(cadena):
@@ -25,3 +28,25 @@ def invertir_cadena(cadena):
     return cadena_invertida
 
 # print(invertir_cadena("hola mundo"))
+
+# Eliminar caracteres duplicados consecutivos.
+
+#     Enunciado: Dada una cadena, elimina todos
+#  los caracteres duplicados consecutivos.
+#     Ejemplo: Entrada: "aabbccdde" -> Salida: "abcde"
+
+def eliminar_duplicados(caracteres):
+    cadena=""
+    for valor in caracteres:
+        if valor not in cadena:
+            cadena+=valor
+
+    return cadena
+
+# print(eliminar_duplicados("aabbccdde"))
+
+# Contar vocales y consonantes en una cadena.
+
+#     Enunciado: Dada una cadena, cuenta cuántas
+#  vocales y cuántas consonantes contiene.
+#     Ejemplo: Entrada: "Hola" -> Salida: Vocales: 2, Consonantes: 2
