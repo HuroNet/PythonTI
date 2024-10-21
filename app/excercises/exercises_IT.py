fb02e42096aee2a02bd2fd162196774d6f339fb9
Huron <charlie.walls97@gmail.com>
2024-10-21 09:06:49 -0500

    no funca

--- app/excercises/exercises_IT.py
+++ app/excercises/exercises_IT.py
@@ -380,9 +380,9 @@ def rotar_lista_derecha(lista, rotar):
 
 # elaborar con indices
 def rotar_con_indices(lista,rotar):
-    resultado=[]* rotar
+    resultado=[] * rotar
     for i in range(rotar):
-        print(i) 
+        resultado[i]= lista[len(lista)-rotar+i]
     return resultado 
 
 print(rotar_con_indices([1,2,3,4,5],2))
