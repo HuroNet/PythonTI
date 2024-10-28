be40e26406a28aee0cb235e50ed89afe96cdfe19
Huron <charlie.walls97@gmail.com>
2024-10-28 07:55:53 -0500

    add exercises of Free code camo to learn JS

---
+++ app/excercises/exercises.js
@@ -0,0 +1,13 @@
+function getAverage(scores) {
+    
+    const tam=scores.length;
+    let  suma=0;
+    for (let i = 0; i < tam; i++) {
+        suma += scores[i]; 
+    }
+    console.log(suma);
+    const resultado = suma / tam;
+    return resultado
+}
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
