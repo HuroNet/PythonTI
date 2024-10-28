9494006dde0f6ecf7ec919079a5708de020f2b03
Huron <charlie.walls97@gmail.com>
2024-10-28 07:57:06 -0500

    more details

--- app/excercises/exercises.js
+++ app/excercises/exercises.js
@@ -5,9 +5,17 @@ function getAverage(scores) {
     for (let i = 0; i < tam; i++) {
         suma += scores[i]; 
     }
-    console.log(suma);
     const resultado = suma / tam;
     return resultado
 }
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+// console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+
+function getGrade(score) {
+
+}
+
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
