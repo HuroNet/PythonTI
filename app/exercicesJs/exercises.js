7b96cd01a72ba48cff1753263918783facefc8ed
Huron <charlie.walls97@gmail.com>
2025-02-04 09:47:29 -0500

    primer ejercicio en js para una prueba tecnica

--- app/exercicesJs/exercises.js
+++ app/exercicesJs/exercises.js
@@ -1,1 +1,14 @@
 // empezamos 
+// Suma de dos números
+
+//     Input: sum(3, 5)
+//     Output: 8
+
+const sumarElementosObjeto=(objeto)=>{
+  console.log(Object.keys(objeto))
+  return 0
+}
+console.log(sumarElementosObjeto({a: 1, b: 2, c: 3}))
+// Entrada: {a: 1, b: 2, c: 3}
+// console.log(sumar_elementos({a: 1, b: 2, c: 3}))
+// Salida: 6
